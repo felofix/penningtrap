a086667c5befff890b6d53748a34f13f4927f4e9
felofix <forsethf@gmail.com>
2022-09-30 07:38:31 +0200

    Started the project

---
+++ penningtrap/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  penningtrap
+//
+//  Created by Felix Aarekol Forseth on 30/09/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
